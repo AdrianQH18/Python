0764001beab76c0102bded88e5f6e171e5e31322
AdrianQH18 <adrian.qh18@gmail.com>
2025-11-28 13:46:58 +0100

    adad
    adada

---
+++ Trimestre2/intro_POO/prueba2.py
@@ -0,0 +1,11 @@
+from abc import abstractmethod, ABCMeta
+
+class ClaseAbstracta(metaclass=ABCMeta):
+    def metodoChorra(self):
+        print("Hola, hola")
+
+    @abstractmethod
+    def metodoAbstracto(self):
+        pass
+nuevo=ClaseAbstracta()
+nuevo.metodoChorra()
