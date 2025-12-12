694b8a2fb2b0a57c4d739e460b273beffc8af19c
AdrianQH18 <adrian.qh18@gmail.com>
2025-12-12 14:20:51 +0100

    ada
    adadad

--- Trimestre1/boletines/boletin2/Ejercicio3.py
+++ Trimestre1/boletines/boletin2/Ejercicio3.py
@@ -1,7 +1,7 @@
 #Escribir un programa que pida un número por teclado al usuario que simule ser el precio
 #de un artículo y escriba el resultado de aplicarle el IVA del 21%. El resultado debe de
 #estar redondeado a dos decimales.
-precio=int(input("Introduce un numero que sera elprecio del producto"))
+precio=int(input("Introduce un numero que sera el precio del producto"))
 iva=precio*0.21
 precioIva=precio+iva
 print(round(precioIva,2))
