1367dd9040d985911c74b9a4183c8e2b9e962a83
AdrianQH18 <adrian.qh18@gmail.com>
2026-01-27 09:32:44 +0100

    basedatos

--- Trimestre2/baseDatos/basededatos.py
+++ Trimestre2/baseDatos/basededatos.py
@@ -1,15 +1,15 @@
-# import mysql.connector
-# try:
-#     #coenctor hacia la base de datos
-#     connect=mysql.connector.connect(user='dam2',password='asdf.1234',host='localhost',database='adat8')
-#     #cursor
-#     cursor=connect.cursor()
-#     cursor.execute("SELECT * FROM Album")
-#     for fila in cursor:
-#         print(fila)
-#
-#     #cierre
-#     cursor.close()
-#     connect.close()
-# except mysql.connector.Error as err:
-#     print(err)
+import mysql.connector
+try:
+    #conector hacia la base de datos
+    connect=mysql.connector.connect(user='dam2',password='asdf.1234',host='localhost',database='adat8')
+    #cursor
+    cursor=connect.cursor()
+    cursor.execute("SELECT * FROM Album")
+    for fila in cursor:
+        print(fila)
+
+    #cierre
+    cursor.close()
+    connect.close()
+except mysql.connector.Error as err:
+    print(err)
